093b65426868f3e50468adf8b0495f247be25fa0
Daniil Bodryagin <daniilMAOmail@gmail.com>
2023-04-04 03:30:35 +0300

    Config script was reworked a bit to delete repetitive code

--- config.js
+++ config.js
@@ -51,12 +51,6 @@ const create = {
                             offsetY: null
                         };
             },
-            getDelta: function(cellY) {
-                const dCellsX = this.class.size.rightLength - (this.properties.coords.cellY - cellY + 1);
-                const dCellsY = this.properties.coords.cellY - cellY + 1;
-                const dCellsHeight = this.class.size.rightLength;
-                return {dCellsX, dCellsY, dCellsHeight};
-            },
             getCellSize: function() {
                 return this.class.size;
             }
@@ -64,458 +58,188 @@ const create = {
     }
 }
 
-export const terrains = [
+function manufactureTerrain (crudeTerrainObject) {
+    const {...terrainObject} = {...crudeTerrainObject};
+    terrainObject.size = {
+        leftLength: 1,
+        rightLength: 1
+    };
+    terrainObject.offset = {
+        x: 0,
+        y: 0
+    };
+    terrainObject.defaultSettings = {};
+    terrainObject.create = create.terrains;
+    terrainObject.getDelta = getDelta;
+    return terrainObject;
+}
+
+function manufactureBall (crudeBallObject) {
+    const {...ballObject} = {...crudeBallObject};
+    ballObject.size = {
+        leftLength: 1,
+        rightLength: 1
+    };
+    ballObject.offset = {
+        x: 9,
+        y: 9
+    };
+    ballObject.defaultSettings = {};
+    ballObject.create = create.environmentals;
+    ballObject.getDelta = getDelta;
+    return ballObject;
+}
+
+const crudeTerrains = [
     {
         name: 'yellow-tile',
         title: 'Yellow Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_00.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_00.png`
     },
     {
         name: 'orange-tile',
         title: 'Orange Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_01.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_01.png`
     },
     {
         name: 'red-tile',
         title: 'Red Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_02.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_02.png`
     },
     {
         name: 'pink-tile',
         title: 'Pink Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_03.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_03.png`
     },
     {
         name: 'hot-pink-tile',
         title: 'Hot Pink Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_04.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_04.png`
     },
     {
         name: 'purple-tile',
         title: 'Purple Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_05.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_05.png`
     },
     {
         name: 'violet-tile',
         title: 'Violet Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_06.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_06.png`
     },
     {
         name: 'dark-blue-tile',
         title: 'Dark Blue Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_07.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_07.png`
     },
     {
         name: 'blue-tile',
         title: 'Blue Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_08.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_08.png`
     },
     {
         name: 'light-blue-tile',
         title: 'Light Blue Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_09.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_09.png`
     },
     {
         name: 'deep-sky-blue-tile',
         title: 'Deep Sky Blue Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_10.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_10.png`
     },
     {
         name: 'aquamarine-tile',
         title: 'Aquamarine Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_11.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_11.png`
     },
     {
         name: 'sea-green-tile',
         title: 'Sea Green Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_12.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_12.png`
     },
     {
         name: 'green-tile',
         title: 'Green Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_13.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_13.png`
     },
     {
         name: 'light-green-tile',
         title: 'Light Green Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_14.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_14.png`
     },
     {
         name: 'dark-green-tile',
         title: 'Dark Green Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_15.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_15.png`
     },
     {
         name: 'brown-tile',
         title: 'Brown Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_16.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_16.png`
     },
     {
         name: 'maroon-tile',
         title: 'Maroon Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_17.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_17.png`
     },
     {
         name: 'teal-tile',
         title: 'Teal Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_18.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_18.png`
     },
     {
         name: 'white-tile',
         title: 'White Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_19.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_19.png`
     },
     {
         name: 'silver-tile',
         title: 'Silver Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_20.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_20.png`
     },
     {
         name: 'light-gray-tile',
         title: 'Light Gray Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_21.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_21.png`
     },
     {
         name: 'gray-tile',
         title: 'Gray Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_22.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_22.png`
     },
     {
         name: 'dark-gray-tile',
         title: 'Dark Gray Tile',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 0,
-            y: 0
-        },
-        source: `${path.terrains}sample_23.png`,
-        defaultSettings: {},
-        create: create.terrains,
-        getDelta: getDelta
+        source: `${path.terrains}sample_23.png`
     }
-]
+];
 
-export const environmentals = [
+const crudeBalls = [
     {
         name: 'orange-ball',
         title: 'Orange Ball',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 9,
-            y: 9
-        },
-        source: `${path.environmentals}ball-orange.png`,
-        defaultSettings: {},
-        create: create.environmentals,
-        getDelta: getDelta
+        source: `${path.environmentals}ball-orange.png`
     },
     {
         name: 'red-ball',
         title: 'Red Ball',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 9,
-            y: 9
-        },
-        source: `${path.environmentals}ball-red.png`,
-        defaultSettings: {},
-        create: create.environmentals,
-        getDelta: getDelta
+        source: `${path.environmentals}ball-red.png`
     },
     {
         name: 'teal-ball',
         title: 'Teal Ball',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 9,
-            y: 9
-        },
-        source: `${path.environmentals}ball-teal.png`,
-        defaultSettings: {},
-        create: create.environmentals,
-        getDelta: getDelta
+        source: `${path.environmentals}ball-teal.png`
     },
     {
         name: 'violet-ball',
         title: 'Violet Ball',
-        size: {
-            leftLength: 1,
-            rightLength: 1
-        },
-        offset: {
-            x: 9,
-            y: 9
-        },
-        source: `${path.environmentals}ball-violet.png`,
-        defaultSettings: {},
-        create: create.environmentals,
-        getDelta: getDelta
-    },
+        source: `${path.environmentals}ball-violet.png`
+    }
+];
+
+export const terrains = crudeTerrains.map(crudeTerrain => manufactureTerrain(crudeTerrain));
+
+export const environmentals = [
+    ...crudeBalls.map(crudeBall => manufactureBall(crudeBall)),
     {
         name: 'shit-pyramid',
         title: 'Badly Made Pyramid',
